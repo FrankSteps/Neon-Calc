27874b16fcfdbf1cc7e6cd349821851d9b66fa99
FrankSteps <franciscopassos.contato@gmail.com>
2025-09-24 18:41:40 -0300

    calc base

--- neon-calc.cpp
+++ neon-calc.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
 
+float val1, val2;
+char simbol; 
+
+int main() {
+    std::cout << "first value: ";
+    std::cin >> val1;
+
+    std::cout << "second value: ";
+    std::cin >> val2;
+
+    std::cout << "operation: ";
+    std::cin >> simbol;
+
+    switch (simbol) {
+        case '+':
+            std::cout << '\n' << val1 + val2 << '\n';
+        break;
+    
+        case '-':
+            std::cout << '\n' << val1 - val2 << '\n';
+        break;
+
+        case '*':
+            std::cout << '\n' << val1 * val2 << '\n';
+        break;
+
+        case '/':
+            if (!val2 == 0) {
+                std::cout << '\n' << val1 + val2 << '\n';
+            } else {
+                std::cout << '\n' << "error" << '\n';
+                return EXIT_FAILURE;
+            }
+        break;
+
+        default:
+            std::cout << '\n' << "error" << '\n';   
+        break;
+    }
+}
